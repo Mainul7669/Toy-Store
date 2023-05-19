6d47f068f850299b204a88d1c8891890d0ff2eda
Mainul7669 <mainul7669@gmail.com>
2023-05-19 20:04:22 +0600

    modal added

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
-import { Table, Form, FormControl, Button } from "react-bootstrap";
+import { Table, Form, FormControl, Button, Modal } from "react-bootstrap";
+
 
 const AllToys = () => {
   const [toys, setToys] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [limit, setLimit] = useState(20);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedToy, setSelectedToy] = useState(null);
+
+
 
   useEffect(() => {
     fetchToys();
@@ -34,19 +39,34 @@ const AllToys = () => {
 
   const limitedToys = filteredToys.slice(0, limit);
 
+  const openModal = (toy) => {
+    setSelectedToy(toy);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div>
+    <div className="d-flex flex-column align-items-center">
       <h1 className="text-center fw-bold mt-3 mb-4">All Toys</h1>
-      <Form inline className="mb-4">
+      <Form className="d-flex mb-4 w-25">
         <FormControl
-          type="text"
+          type="search"
+          className="me-3"
+          aria-label="Search"
           placeholder="Search by Toy Name"
-          className="mr-2"
           value={searchTerm}
           onChange={handleSearch}
         />
-        <Form.Group controlId="limitSelect">
-          <Form.Control as="select" value={limit} onChange={handleLimitChange}>
+        <Form.Group className="w-25">
+          <Form.Control
+            className="fw-bold text-center"
+            as="select"
+            value={limit}
+            onChange={handleLimitChange}
+          >
             <option value={5}>5</option>
             <option value={10}>10</option>
             <option value={20}>20</option>
@@ -54,7 +74,8 @@ const AllToys = () => {
           </Form.Control>
         </Form.Group>
       </Form>
-      <Table striped bordered hover>
+
+      <Table striped bordered hover responsive>
         <thead>
           <tr>
             <th>Seller</th>
@@ -67,19 +88,51 @@ const AllToys = () => {
         </thead>
         <tbody>
           {limitedToys.map((toy) => (
-            <tr key={toy.id}>
-              <td>{toy.seller || "-"}</td>
+            <tr key={toy._id}>
+              <td>{toy.sellerName || "-"}</td>
               <td>{toy.name}</td>
               <td>{toy.subcategory}</td>
               <td>{toy.price}</td>
               <td>{toy.quantity}</td>
               <td>
-                <Button variant="primary">View Details</Button>
+                <div className="d-grid">
+                  <Button
+                    onClick={() => openModal(toy)}
+                    variant="outline-danger"
+                    className="text-dark fw-bold border rounded-3"
+                  >
+                    View Details
+                  </Button>
+                </div>
               </td>
             </tr>
           ))}
         </tbody>
       </Table>
+
+      <Modal show={showModal} onHide={closeModal} centered>
+        {selectedToy && (
+          <>
+            <Modal.Header closeButton>
+              <Modal.Title>{selectedToy.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <img className="w-50" src={selectedToy.picture} alt={selectedToy.name} />
+              <p>Seller: {selectedToy.sellerName || "-"}</p>
+              <p>Seller Email: {selectedToy.sellerEmail || "-"}</p>
+              <p>Price: {selectedToy.price}</p>
+              <p>Rating: {selectedToy.rating}</p>
+              <p>Available Quantity: {selectedToy.quantity}</p>
+              <p>{selectedToy.detailDescription}</p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={closeModal} variant="secondary">
+                Close
+              </Button>
+            </Modal.Footer>
+          </>
+        )}
+      </Modal>
     </div>
   );
 };
